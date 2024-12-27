f29142bfcf87c32e0b4fdbce0777b88332f6592d
Dmytro-Skaliun <dskaliun@gmail.com>
2024-12-27 13:34:29 +0100

    Switched to own auth hostname for sign-in sign-up. Added Example ENV Variables file for the reference

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLauout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLauout;
