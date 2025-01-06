9ba41e347fed5c4cc3c34d71d66cfe55f2cff13c
Dmytro-Skaliun <dskaliun@gmail.com>
2025-01-06 18:36:20 +0100

    Added 3-layer Caching System for Global, User and Id tags

--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,8 +1,44 @@
 import { db } from "@/drizzle/db";
 import { ProductCustomizationTable, ProductTable } from "@/drizzle/schema";
+import {
+  CACHE_TAGS,
+  dbCache,
+  getUserTag,
+  revalidateDbCache,
+} from "@/lib/cache";
 import { and, eq } from "drizzle-orm";
 
+// whenever we modify our db
+// we make sure we call revalidate db cache
+// and we pass it along all the information
+// i.e. when we delete a product:
+//// we know exactly what user that was for
+//// we know exactly what the idea of the product
+//// we know what thing we're tagging
+//// that as in our case - PRODUCT
+//// so we are going to invalidate all the different tags:
+////// individual id, userId (clerkUserId) and the actual tag for the overall Global products
+
+// *** !IMPORTANT *** (fixed in NEXT v15+)
+// we need to make sure to add key to nextConfig object inside "next.config.mjs"
+// =============================================
+// const nextConfig: NextConfig = {
+//   experimental: { staleTimes: { dynamic: 0 } }
+// };
+// =============================================
+
 export function getProducts(userId: string, { limit }: { limit?: number }) {
+  const cacheFn = dbCache(getProductsInternal, {
+    tags: [getUserTag(userId, CACHE_TAGS.products)],
+  });
+
+  return cacheFn(userId, { limit });
+}
+
+export function getProductsInternal(
+  userId: string,
+  { limit }: { limit?: number }
+) {
   return db.query.ProductTable.findMany({
     where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
     orderBy: ({ createdAt }, { desc }) => desc(createdAt),
@@ -14,7 +50,7 @@ export async function createProduct(data: typeof ProductTable.$inferInsert) {
   const [newProduct] = await db
     .insert(ProductTable)
     .values(data)
-    .returning({ id: ProductTable.id });
+    .returning({ id: ProductTable.id, userId: ProductTable.clerkUserId });
 
   try {
     await db
@@ -26,6 +62,12 @@ export async function createProduct(data: typeof ProductTable.$inferInsert) {
   } catch (error) {
     await db.delete(ProductTable).where(eq(ProductTable.id, newProduct.id));
   }
+
+  revalidateDbCache({
+    tag: CACHE_TAGS.products,
+    userId: newProduct.userId,
+    id: newProduct.id,
+  });
 
   return newProduct;
 }
@@ -40,6 +82,14 @@ export async function deleteProduct({
   const { rowCount } = await db
     .delete(ProductTable)
     .where(and(eq(ProductTable.id, id), eq(ProductTable.clerkUserId, userId)));
+
+  if (rowCount > 0) {
+    revalidateDbCache({
+      tag: CACHE_TAGS.products,
+      userId,
+      id,
+    });
+  }
 
   return rowCount > 0; // indicates, if any rows has been deleted
 }
--- src/server/db/users.ts
+++ src/server/db/users.ts
@@ -1,12 +1,39 @@
 import { db } from "@/drizzle/db";
 import { ProductTable, UserSubscriptionTable } from "@/drizzle/schema";
+import { CACHE_TAGS, revalidateDbCache } from "@/lib/cache";
 import { eq } from "drizzle-orm";
 
-export function deleteUser(clerkUserId: string) {
-  return db.batch([
+export async function deleteUser(clerkUserId: string) {
+  const [userSubscriptions, products] = await db.batch([
     db
       .delete(UserSubscriptionTable)
-      .where(eq(UserSubscriptionTable.clerkUserId, clerkUserId)),
-    db.delete(ProductTable).where(eq(ProductTable.clerkUserId, clerkUserId)),
+      .where(eq(UserSubscriptionTable.clerkUserId, clerkUserId))
+      .returning({
+        id: UserSubscriptionTable.id,
+      }),
+    db
+      .delete(ProductTable)
+      .where(eq(ProductTable.clerkUserId, clerkUserId))
+      .returning({
+        id: ProductTable.id,
+      }),
   ]);
+
+  userSubscriptions.forEach((subscription) => {
+    revalidateDbCache({
+      tag: CACHE_TAGS.subscription,
+      userId: clerkUserId,
+      id: subscription.id,
+    });
+  });
+
+  products.forEach((product) => {
+    revalidateDbCache({
+      tag: CACHE_TAGS.products,
+      userId: clerkUserId,
+      id: product.id,
+    });
+  });
+
+  return [userSubscriptions, products];
 }
